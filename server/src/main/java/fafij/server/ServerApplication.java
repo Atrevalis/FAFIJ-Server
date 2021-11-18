ec25b18bfa7090908c59cd5147b292797df7d23e
geniuspopeye <sasha.batalin@gmail.com>
2021-11-19 01:06:02 +0300

    Добавление JWT токенов

--- server/src/main/java/fafij/server/ServerApplication.java
+++ server/src/main/java/fafij/server/ServerApplication.java
@@ -1,18 +1,29 @@
 package fafij.server;
 
-import fafij.server.Repository.UserService;
 import fafij.server.entity.Users;
-import org.springframework.beans.factory.annotation.Autowired;
+import fafij.server.Repository.UserService;
 import org.springframework.boot.SpringApplication;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class ServerApplication {
+    /*@Autowired
+    private UserService userService;*/
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
 
+  /*  @EventListener(ApplicationReadyEvent.class)
+    private void testJpaMethods(){
+        Users user = new Users();
+        user.setLogin("qwerty");
+        user.setPassword("ytrewq");
+        userService.createUsers(user);
+
+        userService.findAllByLogin("qwerty").forEach(it->System.out.println(it));
+    }*/
 }
